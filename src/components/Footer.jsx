337e0b510a38df8087ca30f780343aaedd2866e6
marilynsaravia <goitiasaraviamarilyn@gmail.com>
2024-08-12 16:11:34 +0200

    Add LinkedIn profile link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { getImageFooter } from '../utils'
 const Footer = () => {
     return (
         <footer className='w-full flex justify-center items-center py-3'>
-            <small className='text-center text-sm text-slate-400'>Desarrollado por <a className='text-green-500 hover:text-green-400' href=''>Marilyn Saravia</a></small>
+            <small className='text-center text-sm text-slate-400'>Desarrollado por <a className='text-green-500 hover:text-green-400' href='https://www.linkedin.com/in/marilynsaravia/' target='_blank'>Marilyn Saravia</a></small>
             <img className='w-7 ml-2' src={getImageFooter('bolivia.png')} alt='Bandera boliviana'/>
         </footer>
     )
